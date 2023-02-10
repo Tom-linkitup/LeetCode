1765800e885b259e91897cfa630b7432915d103f
tomgu <atom8708@gmail.com>
2023-02-10 20:08:36 +0000

    two sum

--- src/main/java/org/example/TwoSum.java
+++ src/main/java/org/example/TwoSum.java
@@ -1,4 +1,26 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] intArr = {11, 15, 7, 3};
+        System.out.println(Arrays.toString(twoSum(intArr, 9)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hm.containsKey(nums[i])) {
+                return new int[] {hm.get(nums[i]), i};
+            } else {
+                // find the first element
+                hm.put(target - nums[i], i);
+            }
+        }
+        return new int[] {0,0};
+    }
 }
