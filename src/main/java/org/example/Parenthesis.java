353ef1bbbef55956cdac99499a2cbc7e20bcda4f
tomgu <atom8708@gmail.com>
2023-02-10 19:36:22 +0000

    valid parenthesis

---
+++ src/main/java/org/example/Parenthesis.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.Stack;
+
+public class Parenthesis {
+
+    public static void main(String[] args) {
+
+        String test1 = "))((((())";
+        int result1 = countValidParenthesis(test1);
+        if (result1 == 0) System.out.println("NO!");
+        else System.out.println("YES! Count: " + result1);
+
+        String test2 = "()()()()(())(()())";
+        int result2 = countValidParenthesis(test2);
+        if (result2 == 0) System.out.println("NO!");
+        else System.out.println("YES! Count: " + result2);
+    }
+
+    public static int countValidParenthesis(String str) {
+
+        Stack<Character> st = new Stack<>();
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(') {
+                st.push(ch);
+            } else if (ch == ')') {
+                if (st.isEmpty()) return 0;
+                Character pop = st.pop();
+                if (pop == null) {
+                    return 0;
+                } else {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
