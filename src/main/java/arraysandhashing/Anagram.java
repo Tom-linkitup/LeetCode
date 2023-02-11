19d5c519248da2dbbe3c4768a41fb28bcbb0b47b
tomgu <atom8708@gmail.com>
2023-02-11 16:41:01 +0000

    valid anagram

---
+++ src/main/java/arraysandhashing/Anagram.java
@@ -0,0 +1,23 @@
+package arraysandhashing;
+
+import java.util.Arrays;
+
+public class Anagram {
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(isAnagram(s, t));
+    }
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        return Arrays.equals(sArray, tArray);
+    }
+}
--- src/main/java/arraysandhashing/TwoSum.java
+++ src/main/java/arraysandhashing/TwoSum.java
@@ -1,4 +1,4 @@
-package org.example;
+package arraysandhashing;
 
 import java.util.Arrays;
 import java.util.HashMap;
