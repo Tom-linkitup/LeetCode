7e9fcf233133dc229c9d521af289f975ceea6651
tomgu <atom8708@gmail.com>
2023-02-15 22:05:35 +0000

    valid Sudoku

--- src/main/java/binarysearch/Search2DMatrix.java
+++ src/main/java/binarysearch/Search2DMatrix.java
@@ -4,7 +4,7 @@ public class Search2DMatrix {
 
     public static void main(String[] args) {
         int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
-        searchMatrix(matrix, 3);
+        System.out.println(searchMatrix(matrix, 3));
     }
 
     public static boolean searchMatrix(int[][] matrix, int target) {
